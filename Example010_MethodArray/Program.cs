97d4f443c84e78d2633b11c9db5a25686f82dd49
Kate <kate.klimenkova@mail.ru>
2023-09-25 11:10:55 +0300

    second commit

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 16, 23, 36, 46, 52, 46, 16, 79, 78 };
+int n = array.Length;
+int find = 46;
+
+int index = 0;
+
+while (index<n) 
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
